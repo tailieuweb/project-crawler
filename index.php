c837fa6df7502ccc9aefa7afa0644704b39fbe9a
Trong Nguyen <trongnguyendev@gmail.com>
2021-03-12 07:36:54 +0700

    commit 12/3 - update parse.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$url= 'https://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-processing-an-unsorted-array';
+$url= 'https://www.facebook.com/groups/499467900233207';
 $html = file_get_contents($url);
-file_put_contents("detail.html", $html);
+file_put_contents("groupfb.html", $html);
 var_dump($html);
 ?>
--- parse.php
+++ parse.php
@@ -1,9 +1,8 @@
 <?php 
 
-$pattern = '/user-gravatar32[\S\s]*?<img.*?src="(.*?)"/';
-$content = file_get_contents('http://localhost/project-crawler/tagged.html');
+$pattern = '/header[\S\s]*?<a\shref="(.*?)"/';
+$content = file_get_contents('http://localhost/project-crawler/tag.html');
 preg_match_all($pattern, $content, $out);
-var_dump($out);
 
 // if(isset($_POST['submit'])) {
     
@@ -18,12 +17,22 @@ var_dump($out);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    <!-- <form action="" method="post">
-        <input type="text" name="pre" id="" value="/summary[\S\s]*?<a.*?question-hyperlink.>(.*?)<\/a>/"><br><br>
-        <textarea name="content" id="" cols="30" rows="10" value="<?php echo file_get_contents('https://stackoverflow.com/questions/tagged/angular'); ?>"></textarea>
-        <input type="submit" value="Submit" name="submit">
-    </form> -->
+    <div id="pattern_form">
+            <div class="header-form">
+                <h1>Contactanos</h1>
+                <p>Con gusto te ayudaremos</p>
+            </div>
+            <form action="" method="POST" name="formRegular">
+                <input type="text" name="regularText" id="" placeholder="Regular Expression">
+                <input type="text" name="regularFile" id="file_regular" placeholder="Name file..">
+                <textarea name="" id="" cols="30" rows="10" placeholder="HTML.."></textarea>
+                <button id="btn-pattern" name="submit-regular">Submit</button>
+            </form>
+            
+                
+        </div>
 </body>
 </html>
